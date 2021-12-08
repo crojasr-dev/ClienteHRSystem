9594fa252393aaafa7ce4f920330c30442f71182
Cristian Rojas <crojasr@outlook.com>
2021-12-07 21:14:08 -0300

    se agregar lista de empleados desde la api

--- src/app/lista-evaluados/lista-evaluados.component.ts
+++ src/app/lista-evaluados/lista-evaluados.component.ts
@@ -1,25 +1,33 @@
-import { Component } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-lista-evaluados',
   templateUrl: './lista-evaluados.component.html',
   styleUrls: ['./lista-evaluados.component.css']
 })
-export class ListaEvaluadosComponent {
 
-  empleados = ["Juan Perez", "Jorge Inostroza", "Roberto Kakas"]
+export class ListaEvaluadosComponent implements OnInit {
 
-  ObtenerDatos = async () => {
-    const resp = await fetch('http://hrapp.developman.net/Api/Personas', {
-      'mode': 'no-cors',
-      'headers': {
-        'Access-Control-Allow-Origin': '*',
-      }
-    });
-    const data = await resp;
-    console.log(data);
-
+  constructor(private peticion: HttpClient) { }
+  ngOnInit(): void {
+   this.ObtenerDatos();
   }
+  empleados: Persona[] = [];
+
+  ObtenerDatos() {
+    // this.peticion.get('http://hrapp.developman.net/Api/Personas')
+    this.peticion.get<Persona[]>('https://localhost:44336//Api/Personas')
+      .subscribe(data => {
+        this.empleados = data.sort();
+      })
+  }
+
+}
+export interface Persona {
+  ID: number;
+  NOMBRE: string;
+  RUT: string;
+  AREA: string;
+  CARGO: string;
 }
